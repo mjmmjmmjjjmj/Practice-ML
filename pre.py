8d74f4a18edafcfaab80207a3dcdc76f0d29f176
mjmmjmmjjjmj <160682854+mjmmjmmjjjmj@users.noreply.github.com>
2024-07-29 10:03:43 +0900

    Add files via upload

---
+++ pre.py
@@ -0,0 +1,31 @@
+from IPython.display import set_matplotlib_formats, display
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import mglearn
+
+from mglearn import plot_helpers
+from cycler import cycler
+
+set_matplotlib_formats('pdf', 'png')
+
+plt.rcParams['savefig.dpi'] = 300
+plt.rcParams['figure.dpi'] = 100
+plt.rcParams['image.cmap'] = "viridis"
+plt.rcParams['image.interpolation'] = "none"
+plt.rcParams['savefig.bbox'] = "tight"
+plt.rcParams['lines.linewidth'] = 1
+plt.rcParams['legend.numpoints'] = 1
+plt.rc('axes', prop_cycle=(
+    cycler('color', plot_helpers.cm_cycle.colors) +
+    cycler('linestyles', ['-', '-', '--',
+                          (0, (3, 3)), (0, (1.5, 1.5))]
+    
+    ))                      
+)
+np.set_printoptions(precision=4, suppress=True)
+pd.set_option('display.max_columns', 8)
+pd.set_option('display.precision', 2)
+
+__all__ = ['np', 'mglearn', 'display', 'plt', 'pd']
